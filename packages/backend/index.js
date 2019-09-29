05d73354a4f93525395ced2eac267cfa67a9a7b8
Hung Nguyen <nthungdev@gmail.com>
2019-09-29 16:21:42 -0400

    Match the backend to firebase configuration

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions')
+const express = require('express')
 const expressApp = require('./express/app')
 
 // // Create and Deploy Your Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-// Create "api" function to host all other top-level endpoints from Express
-exports.api = functions.https.onRequest(expressApp)
+// Create "api" wrapper to host all other top-level endpoints from Express
+const api = express()
+api.use('/api', expressApp)
+exports.api = functions.https.onRequest(api)
