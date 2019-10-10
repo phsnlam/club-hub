f18581cbfa64086d9eeb3f1122f0a461bf0a1273
Michael Barbas <michaelbarbas@gmail.com>
2019-10-10 19:53:56 -0400

    Added review model

--- packages/backend/common/models/review.js
+++ packages/backend/common/models/review.js
@@ -1,9 +1,19 @@
 const Joi = require('@hapi/joi')
+const {id} = require('./user') 
 
 const ReviewSchema = Joi.object({
-  id: Joi.string()
-    .alphanum()
-    .required()
+  id: id,
+
+  date: Joi.date().timestamp().required(),
+
+  user_id: Joi.string().required(),
+
+  detail: Joi.string().required(),
+
+  //rating in stars(int 0-5) 
+  rating: Joi.number().required(),
+
+  club_id: Joi.string().required()
 })
 
 module.exports = ReviewSchema
