1328609f6c30fc4e312aec2e82e8cc88ef180359
Michael Barbas <michaelbarbas@gmail.com>
2019-10-18 16:40:50 -0400

    added min and max to rating, simplified user_id, 'future proofed' review ID

--- packages/backend/common/models/review.js
+++ packages/backend/common/models/review.js
@@ -2,16 +2,18 @@ const Joi = require('@hapi/joi')
 const {id} = require('./user') 
 
 const ReviewSchema = Joi.object({
-  id: id,
+  id: Joi.string()
+  .alphanum()
+  .required(), //same as 'id', didnt use id from user schema in case we change pattern
 
   date: Joi.date().timestamp().required(),
 
-  user_id: Joi.string().required(),
+  user_id: id,
 
   detail: Joi.string().required(),
 
-  //rating in stars(int 0-5) 
-  rating: Joi.number().required(),
+  //rating in stars(min 1 stars - max 5 stars) 
+  rating: Joi.number().min(1).max(5).required(),
 
   club_id: Joi.string().required()
 })
