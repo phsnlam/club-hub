4c3fe3d2a2ae128b1dac3da756e907df8b4c16ae
Michael Barbas <michaelbarbas@gmail.com>
2019-10-27 14:59:26 -0400

    created async func. addClub, which validates and sends the data of the newclub

--- packages/backend/express/routes/club.js
+++ packages/backend/express/routes/club.js
@@ -1,18 +1,21 @@
 const express = require('express')
+const Joi = require('@hapi/joi')
 const router = express.Router()
 const admin = require('firebase-admin');
 const { ClubSchema } = require('@clubhub/common/models');
 
 
 /* GET users listing. */
-router.post('/', async (req, res, next) => {
-    const db = admin.firestore();
-
-
-    await db.collection('users').add({
-        id : '1241'
-    })
+router.post('/', (req, res, next) => { // , async (req..) =>
+    //const db = admin.firestore();
+    //await db.collection('clubs').add({
+        
+    //})
     res.send('respond with a resource')
+    const result = ClubSchema.validate(req.body);
+    res.send(result);
 })
+
+
 
 module.exports = router
