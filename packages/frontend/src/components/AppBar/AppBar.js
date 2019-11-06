8718af5930b6cef47b317d26fbf16cb2ffa6e843
Jake <phsnlam@gmail.com>
2019-11-05 23:50:36 -0500

    Messed around a bit
    Working on the navigation bar

--- packages/frontend/src/components/AppBar/AppBar.js
+++ packages/frontend/src/components/AppBar/AppBar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
 import styles from './AppBar.module.css'
 import { Link } from 'react-router-dom'
 import { SearchBar } from '@clubhub/components'
@@ -6,26 +7,63 @@ import { SearchBar } from '@clubhub/components'
 
 const AppBar = () => {
   return (
-    <div className={styles.root}>
-      <SearchBar/>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/setting">Setting</Link>
-          </li>
-        </ul>
-      </div>
+    <div className = "navBar">
+      <div><Dropdown/></div>
+      <div><SearchBar/></div>
     </div>
   )
+}
+
+
+class Dropdown extends Component {
+constructor(){
+ super();
+
+ this.state = {
+       displayMenu: false,
+     };
+
+  this.showDropdownMenu = this.showDropdownMenu.bind(this);
+  this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+
+};
+
+showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+    document.addEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  hideDropdownMenu() {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+
+  }
+
+  render() {
+    return (
+        <div  className="dropdown">
+         <button className="button" onClick={this.showDropdownMenu}>Account Options</button>
+
+          { this.state.displayMenu ? (
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/setting">Setting</Link></li>
+          </ul>
+        ):
+        (
+          null
+        )
+        }
+
+       </div>
+
+    );
+  }
 }
 
 export default AppBar
--- packages/frontend/src/pages/HomePage/HomePage.js
+++ packages/frontend/src/pages/HomePage/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import styles from './HomePage.module.css'
 
+
 const HomePage = () => {
-  return <div className={styles.root}>Home Page</div>
+  return (
+  null
+  )
 }
 
 export default HomePage
